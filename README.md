0e0984a9401d413b0cf7d81dad415a71d28e4f06
AllyGarcia <allygarcia152@gmail.com>
2022-02-26 17:50:36 -0600

    created random for loop that takes user criteria to generate a random password

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Password Generator Starter Code
+## Description
+Employees of X Company has access to sensitive data, and needs to randomly generate a secure password. This password generator allows employees of this company to generate a random password to the length they desire between 8 and 128 characters. Users can choose if their password inlcudes uppercase, lowercase, numbers, and/or special characters
+
+## Table of Contents
+1. [Usage](#usage)
+2. [Updates](#updates)
+3. [Liscense](#liscense)
+
+## Usage 
+Users for this generator can click the "generate password" button that will trigger a pop up window that first asks the user how long they want their password to be. If the password is not a number at all or not a number between 8 and 128 the window will prompt the user to enter acceptabple criteria. It will then ask for other password criteria. If the user enters cancel for all criteria, a pop up window will tell the user to choose at least one password criteria and the user will have to generate the password all over again by clicking the generate password button. 
+
+![gif of Allyson's online profile](./assets/images/AllyG-Portfolio.gif)
+
+link: [AllyG - Profile](https://allygarcia152.github.io/UTSAchallenge2/)
+
+## Updates
+The generate password website came styled fully but the "generate password" button would not generate a random password. The JS file has been updated to take in user criteria for a password and give them one in the text box above the button. 
+
+## Liscense
+:)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -12,56 +9,88 @@ function writePassword() {
   passwordText.value = password;
 };
 
-//  ask the user how long they want the password to be
-var getPasswordLength = function(){
+//get password criteria
+function getPasswordCriteria(){
+  //  ask the user how long they want the password to be
   var length = parseInt(prompt("Please enter how long you would like your password to be."));
   //ask again if the password length does not meet criteria
-  if (length < 8 || length > 128 || isNaN(length)){
+  while (length < 8 || length > 128 || isNaN(length)){
     alert("Please enter a length between 8 and 128 characters.");
-    return null;
+    length = parseInt(prompt("Please enter how long you would like your password to be."));
   }
-  console.log("Your password length is " + length + " characters long!");
   // alert user of password length
   alert("Your password length is " + length + " characters long!");
 
-  return length;
-};
-
-// ask user if they want upper case characters in their password
-var getUpperCase = function (){
+  // ask user if they want upper case characters in their password
   var upperCase = confirm("Would you like upper case letters in your password?");
-  return upperCase;
-};
 
-// ask user if they want lower case characters in their password
-var getLowerCase = function (){
+  // ask user if they want lower case characters in their password
   var lowerCase = confirm("Would you like lower case letters in your password?");
-  return lowerCase;
-};
 
-// ask user if they want UC characters in their password
-var getNumbers = function (){
+  // ask user if they want UC characters in their password
   var numbers = confirm("Would you like numbers in your password?");
-  return numbers;
-};
-
-// ask user if they want special characters in their password
-var getSpecialCharacters = function (){
-  var specialCharacters = confirm("Would you like special characters in your password?");
-  return specialCharacters;
-};
-
-var passwordCriteria = {
-  length: getPasswordLength(),
-  upperCase: getUpperCase (),
-  lowerCase: getLowerCase (),
-  numbers: getNumbers (),
-  specialCharacters: getSpecialCharacters (),
-};
-
-function generatePassword(event){
   
+
+  // ask user if they want special characters in their password
+  var specialCharacters = confirm("Would you like special characters in your password?");
+ 
+  //ask user to generate a new password if they click cancell on all criteria
+  if (lowerCase === false && upperCase === false && specialCharacters === false && numbers=== false){
+    alert("You must choose at least one password criteria to generate your secure password. Please try again.")
+    return null; 
+  };
+
+  //object for all password criteria
+  var passwordCriteria = {
+    length: length,
+    upperCase: upperCase,
+    lowerCase:lowerCase,
+    numbers: numbers,
+    specialCharacters: specialCharacters,
+  };
+  return passwordCriteria;
 };
+
+// execute generate password function when generate button is clicked
+function generatePassword(event){
+  var passwordCriteria = getPasswordCriteria();
+  console.log(passwordCriteria);
+
+  //create strings to pull random password from
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var specialCharacters = "~!@#$%^&*()-+<>?/_";
+
+  var password = "";
+
+  //create a pool of characters to choose from based on the user's choices
+  var characters = "";
+  if (passwordCriteria.upperCase) {
+    characters += upperCase;
+  };
+  if (passwordCriteria.lowerCase){
+    characters += lowerCase;
+  };
+  if (passwordCriteria.numbers) {
+    characters += numbers;
+  };
+  if (passwordCriteria.specialCharacters){
+    characters += specialCharacters;
+  };
+  //get a random character from the accepted criteria for as long as the length chosen by the user
+  for (var i=0; i < passwordCriteria.length; i++){
+    var character = characters.charAt(Math.floor(Math.random()*characters.length));
+    password += character;
+  };
+
+  return password;
+};
+
+//grab a randomcharacter from the pool of characters in the user's pw criteria
+// function getRandomCharacter(string){
+//   var charactersArray = string.split("");
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
